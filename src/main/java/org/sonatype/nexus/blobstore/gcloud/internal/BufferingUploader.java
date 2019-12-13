83a20b4e5fb170dbe1e7520c9c4de97a4671783d
Nicholas Blair <nblair@sonatype.com>
2019-12-12 20:56:36 -0600

    on-thread alternative to MultipartUploader
    The BufferingUploader follows the same contract as MultipartUploader to break content into fixed size chunks and upload with gzip compression disabled. The storage writes and compose request all occur in serial on the main thread.
    
    One benefit to this option is that there are no calls to Arrays.copyOfRange; every chunk uses the exact same byte[] buffer, so this may have a better memory profile. The runtimes on integration tests are slower than MultipartUpload, so isolating to a separate feature branch at this time.

---
+++ src/main/java/org/sonatype/nexus/blobstore/gcloud/internal/BufferingUploader.java
@@ -0,0 +1,225 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2017-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.blobstore.gcloud.internal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.sonatype.nexus.blobstore.api.BlobStoreException;
+import org.sonatype.nexus.common.stateguard.StateGuardLifecycleSupport;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.Storage.BlobTargetOption;
+import com.google.cloud.storage.Storage.ComposeRequest;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+/**
+ * Component that provides parallel multipart upload support for blob binary data (.bytes files).
+ */
+@Named
+public class BufferingUploader
+    extends StateGuardLifecycleSupport
+{
+
+  /**
+   * Use this property in 'nexus.properties' to control how large each multipart part is. Default is 5 MB.
+   * Smaller numbers increase the number of parallel workers used to upload a file. Match to your workload:
+   * if you are heavy in docker with large images, increase; if you are heavy in smaller components, decrease.
+   */
+  public static final String CHUNK_SIZE_PROPERTY = "nexus.gcs.multipartupload.chunksize";
+
+  /**
+   * This is a hard limit on the number of components to a compose request enforced by Google Cloud Storage API.
+   */
+  static final int COMPOSE_REQUEST_LIMIT = 32;
+
+  /**
+   * While an invocation of {@link #upload(Storage, String, String, InputStream)} is in-flight, the individual
+   * chunks of the file will have names like 'destination.chunkPartNumber", like
+   * 'content/vol-01/chap-01/UUID.bytes.chunk1', 'content/vol-01/chap-01/UUID.bytes.chunk2', etc.
+   */
+  private final String CHUNK_NAME_PART = ".chunk";
+
+  /**
+   * Used internally to count how many times we've hit the compose limit.
+   * Consider exposing this as a bean that can provide tuning feedback to deployers.
+   */
+  private final AtomicLong composeLimitHit = new AtomicLong(0);
+
+  private static final byte[] EMPTY = new byte[0];
+
+  private final ListeningExecutorService executorService = MoreExecutors.listeningDecorator(
+      Executors.newSingleThreadExecutor(
+          new ThreadFactoryBuilder().setNameFormat("nexus-google-cloud-storage-multipart-upload-%d").build()));
+
+  private final int chunkSize;
+
+  @Inject
+  public BufferingUploader(@Named("${"+CHUNK_SIZE_PROPERTY +":-5242880}") final int chunkSize) {
+    this.chunkSize = chunkSize;
+  }
+
+  /**
+   * @return the value for the {@link #CHUNK_SIZE_PROPERTY}
+   */
+  public int getChunkSize() {
+    return chunkSize;
+  }
+
+  /**
+   * @return the number of times {@link #upload(Storage, String, String, InputStream)} hit the multipart-compose limit
+   */
+  public long getNumberOfTimesComposeLimitHit() {
+    return composeLimitHit.get();
+  }
+
+  /**
+   * @param storage an initialized {@link Storage} instance
+   * @param bucket the name of the bucket
+   * @param destination the the destination (relative to the bucket)
+   * @param contents the stream of data to store
+   * @return the successfully stored {@link Blob}
+   * @throws BlobStoreException if any part of the upload failed
+   */
+  public Blob upload(final Storage storage, final String bucket, final String destination, final InputStream contents) {
+    log.debug("Starting multipart upload for destination {} in bucket {}", destination, bucket);
+    // this must represent the bucket-relative paths to the chunks, in order of composition
+    List<String> chunkNames = new ArrayList<>();
+
+    Optional<Blob> singleChunk = Optional.empty();
+    try (InputStream current = contents) {
+
+      // MUST respect hard limit of 32 chunks per compose request
+      for (int partNumber = 1; partNumber <= COMPOSE_REQUEST_LIMIT; partNumber++) {
+        final byte[] buffer = new byte[chunkSize];
+
+        if (partNumber == 1) {
+          int length = bufferChunk(current, buffer);
+          final String chunkName = toChunkName(destination, partNumber);
+          chunkNames.add(chunkName);
+
+          BlobInfo blobInfo = BlobInfo.newBuilder(bucket, chunkName).build();
+          Blob blob = storage.create(blobInfo, buffer, 0, length, BlobTargetOption.disableGzipContent());
+          singleChunk = Optional.of(blob);
+        }
+        else if (partNumber > 1 && partNumber < COMPOSE_REQUEST_LIMIT)
+        {
+          int length = bufferChunk(current, buffer);
+          if (length == 0 ) {
+            break;
+          }
+          // we've got more than one chunk; flag this back to empty so we get compose request at the end
+          singleChunk = Optional.empty();
+          final String chunkName = toChunkName(destination, partNumber);
+          chunkNames.add(chunkName);
+
+          BlobInfo blobInfo = BlobInfo.newBuilder(bucket, chunkName).build();
+          log.debug("Uploading chunk {} for {} of {} bytes", partNumber, destination, length);
+          Blob blob = storage.create(blobInfo, buffer, 0, length, BlobTargetOption.disableGzipContent());
+        }
+        else {
+          log.debug("Upload for {} has hit Google Cloud Storage multipart-compose limits; " +
+              "consider increasing '{}' beyond current value of {}", destination, CHUNK_SIZE_PROPERTY, getChunkSize());
+          // we've hit compose request limit read the rest of the stream
+          composeLimitHit.incrementAndGet();
+
+          final String finalChunkName = toChunkName(destination, COMPOSE_REQUEST_LIMIT);
+          chunkNames.add(finalChunkName);
+
+          log.debug("Uploading final chunk {} for {} of unknown remaining bytes", COMPOSE_REQUEST_LIMIT, destination);
+          BlobInfo blobInfo = BlobInfo.newBuilder(
+              bucket, finalChunkName).build();
+          // read the rest of the current stream
+          // downside here is that since we don't know the stream size, we can't chunk it.
+          // the deprecated create here does not allow us to disable GZIP compression on these PUTs
+          Blob lastChunk = storage.create(blobInfo, current);
+        }
+      }
+
+      // return the single result if it exists; otherwise finalize the parallel multipart workers
+      return singleChunk.orElseGet(() -> {
+        Blob finalBlob = storage.compose(ComposeRequest.of(bucket, chunkNames, destination));
+        log.debug("Multipart upload of {} complete", destination);
+        return finalBlob;
+      });
+    }
+    catch(Exception e) {
+      throw new BlobStoreException("Error uploading blob", e, null);
+    } finally {
+      // remove any .chunkN files off-thread
+      // make sure not to delete the first chunk (which has the desired destination name with no suffix)
+      deferredCleanup(storage, bucket, chunkNames);
+    }
+  }
+
+  private void deferredCleanup(final Storage storage, final String bucket, final List<String> chunkNames) {
+    executorService.submit(() -> chunkNames.stream()
+        .filter(part -> part.contains(CHUNK_NAME_PART))
+        .forEach(chunk -> storage.delete(bucket, chunk)));
+  }
+
+  /**
+   * The name of the first chunk should match the desired end destination.
+   * For any chunk index 2 or greater, this method will return the destination + the chunk name suffix.
+   *
+   * @param destination
+   * @param chunkNumber
+   * @return the name to store this chunk
+   */
+  private String toChunkName(String destination, int chunkNumber) {
+    if (chunkNumber == 1) {
+      return destination;
+    }
+    return destination + CHUNK_NAME_PART + chunkNumber;
+  }
+
+  /**
+   * Read a chunk of the stream up to {@link #getChunkSize()} into the provided buffer.
+   *
+   * @param input the stream to read
+   * @param buffer the byte[] to buffer the contents
+   * @return the length of bytes read
+   * @throws IOException
+   */
+  private int bufferChunk(final InputStream input, byte[] buffer) throws IOException {
+    int offset = 0;
+    int remain = chunkSize;
+    int bytesRead = 0;
+
+    while (remain > 0 && bytesRead >= 0) {
+      bytesRead = input.read(buffer, offset, remain);
+      if (bytesRead > 0) {
+        offset += bytesRead;
+        remain -= bytesRead;
+      }
+    }
+    return offset;
+  }
+}
---
+++ src/test/java/org/sonatype/nexus/blobstore/gcloud/internal/BufferingUploaderIT.groovy
@@ -0,0 +1,217 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2017-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.blobstore.gcloud.internal
+
+import java.util.stream.StreamSupport
+
+import org.sonatype.nexus.blobstore.api.BlobStoreConfiguration
+
+import com.google.cloud.storage.Blob
+import com.google.cloud.storage.Blob.BlobSourceOption
+import com.google.cloud.storage.BucketInfo
+import com.google.cloud.storage.Storage
+import groovy.util.logging.Slf4j
+import org.apache.commons.io.input.BoundedInputStream
+import spock.lang.Specification
+
+@Slf4j
+class BufferingUploaderIT
+    extends Specification
+{
+
+  static final BlobStoreConfiguration config = new BlobStoreConfiguration()
+
+  static final GoogleCloudStorageFactory storageFactory = new GoogleCloudStorageFactory()
+
+  static String bucketName = "integration-test-${UUID.randomUUID().toString()}"
+
+  static Storage storage
+
+  def setupSpec() {
+    config.attributes = [
+        'google cloud storage': [
+            bucket: bucketName,
+            credential_file: this.getClass().getResource('/gce-credentials.json').getFile()
+        ]
+    ]
+
+    log.info("Integration test using bucket ${bucketName}")
+    storage = storageFactory.create(config)
+    storage.create(BucketInfo.of(bucketName))
+  }
+
+  def cleanupSpec() {
+    //Storage storage = new GoogleCloudStorageFactory().create(config)
+    log.debug("Tests complete, deleting files from ${bucketName}")
+    // must delete all the files within the bucket before we can delete the bucket
+    Iterator<com.google.cloud.storage.Blob> list = storage.list(bucketName,
+        Storage.BlobListOption.prefix("")).iterateAll()
+        .iterator()
+
+    Iterable<com.google.cloud.storage.Blob> iterable = { _ -> list }
+    StreamSupport.stream(iterable.spliterator(), true)
+        .forEach({ b -> b.delete(BlobSourceOption.generationMatch()) })
+    storage.delete(bucketName)
+    log.info("Integration test complete, bucket ${bucketName} deleted")
+  }
+
+  def "simple multipart"() {
+    given:
+      long expectedSize = (1048576 * 3) + 2
+      BufferingUploader uploader = new BufferingUploader(1048576)
+      byte[] data = new byte[expectedSize]
+      new Random().nextBytes(data)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName, 'vol-01/chap-01/control/multi_part', new ByteArrayInputStream(data))
+
+    then:
+      blob.size == expectedSize
+      storage.get(bucketName, 'vol-01/chap-01/control/multi_part').getContent() == data
+  }
+
+  def "confirm parts composed in order"() {
+    given:
+      // 5 each of abcdefg
+      final String content =  "aaaaabbbbbcccccdddddeeeeefffffggggg"
+      byte[] data = content.bytes
+      BufferingUploader uploader = new BufferingUploader(5)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName, 'vol-01/chap-01/control/in_order', new ByteArrayInputStream(data))
+
+    then:
+      blob.size == data.length
+      Blob readback = storage.get(blob.blobId)
+      readback.getContent() == content.bytes
+  }
+
+  def "single part"() {
+    given:
+      long expectedSize = 1048575
+      BufferingUploader uploader = new BufferingUploader(1048576)
+      byte[] data = new byte[expectedSize]
+      new Random().nextBytes(data)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName, 'vol-01/chap-01/control/single_part', new ByteArrayInputStream(data))
+
+    then:
+      blob.size == expectedSize
+      storage.get(bucketName, 'vol-01/chap-01/control/single_part').getContent() == data
+  }
+
+  def "zero byte file"() {
+    given:
+      long expectedSize = 0
+      BufferingUploader uploader = new BufferingUploader(1024)
+      byte[] data = new byte[expectedSize]
+      new Random().nextBytes(data)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName, 'vol-01/chap-01/control/zero_byte', new ByteArrayInputStream(data))
+
+    then:
+      blob.size == expectedSize
+      storage.get(bucketName, 'vol-01/chap-01/control/zero_byte').getContent() == data
+  }
+
+  def "hit compose limit slightly and still successful"() {
+    given:
+      long expectedSize = (1024 * MultipartUploader.COMPOSE_REQUEST_LIMIT) + 10
+      BufferingUploader uploader = new BufferingUploader(1024)
+      byte[] data = new byte[expectedSize]
+      new Random().nextBytes(data)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName,
+          'vol-01/chap-01/composeLimitTest/small_miss', new ByteArrayInputStream(data))
+
+    then:
+      blob.size == expectedSize
+      uploader.numberOfTimesComposeLimitHit == 1L
+      storage.get(bucketName, 'vol-01/chap-01/composeLimitTest/small_miss').getContent() == data
+  }
+
+  def "hit compose limit poorly tuned, still successful" () {
+    given:
+      long expectedSize = 1048576
+      BufferingUploader uploader = new BufferingUploader(1024)
+      byte[] data = new byte[expectedSize]
+      new Random().nextBytes(data)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName,
+          'vol-01/chap-01/composeLimitTest/poor_tuning', new ByteArrayInputStream(data))
+
+    then:
+      blob.size == expectedSize
+      uploader.numberOfTimesComposeLimitHit == 1L
+      storage.get(bucketName, 'vol-01/chap-01/composeLimitTest/poor_tuning').getContent() == data
+  }
+
+  /**
+   * Larger upload that still fits ideally within our default tuning. 100 MB will result in 20 5 MB chunks.
+   */
+  def "upload 100 MB"() {
+    given:
+      long expectedSize = 1024 * 1024 * 100
+      BoundedInputStream inputStream = new BoundedInputStream(new InputStream() {
+        private Random random = new Random()
+        @Override
+        int read() throws IOException {
+          return random.nextInt()
+        }
+      }, expectedSize)
+      // default value of 5 MB per chunk
+      BufferingUploader uploader = new BufferingUploader(1024 * 1024 * 5)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName,
+          'vol-01/chap-02/large/one_hundred_MB', inputStream)
+    then:
+      blob.size == expectedSize
+      storage.get(bucketName, 'vol-01/chap-02/large/one_hundred_MB').size == expectedSize
+  }
+
+  /**
+   * The difference in this test beyond the 'upload 100 MB' test is that the upload will:
+   *
+   * a) result in incrementing {@link MultipartUploader#getNumberOfTimesComposeLimitHit()} and
+   * b) the last chunk will be significantly larger than the preceding 31.
+   *
+   * This represents a situation where the customer may need to adjust their chunk size upward. The larger final chunk
+   * may also elicit runtime pressure on memory.
+   */
+  def "upload 200 MB"() {
+    given:
+      long expectedSize = 1024 * 1024 * 200
+      BoundedInputStream inputStream = new BoundedInputStream(new InputStream() {
+        private Random random = new Random()
+        @Override
+        int read() throws IOException {
+          return random.nextInt()
+        }
+      }, expectedSize)
+      // with value of 5 MB per chunk, we'll upload 31 5 MB chunks and 1 45 MB chunk
+      BufferingUploader uploader = new BufferingUploader(1024 * 1024 * 5)
+
+    when:
+      Blob blob = uploader.upload(storage, bucketName,
+          'vol-01/chap-02/large/two_hundred_MB', inputStream)
+    then:
+      blob.size == expectedSize
+      uploader.getNumberOfTimesComposeLimitHit() == 1L
+      storage.get(bucketName, 'vol-01/chap-02/large/two_hundred_MB').size == expectedSize
+  }
+}
