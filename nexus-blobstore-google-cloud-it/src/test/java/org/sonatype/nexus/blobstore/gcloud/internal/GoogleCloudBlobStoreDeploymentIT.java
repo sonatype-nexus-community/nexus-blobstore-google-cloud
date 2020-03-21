378e57670e08a23bfa32712a011617898e3e2cca
Nicholas Blair <nblair@sonatype.com>
2020-03-21 08:58:30 -0500

    add osgi-specific dependencies for httpcore and httpclient
    Solves classloading issue that plagued the prior commit.

--- nexus-blobstore-google-cloud-it/src/test/java/org/sonatype/nexus/blobstore/gcloud/internal/GoogleCloudBlobStoreDeploymentIT.java
+++ nexus-blobstore-google-cloud-it/src/test/java/org/sonatype/nexus/blobstore/gcloud/internal/GoogleCloudBlobStoreDeploymentIT.java
@@ -1,10 +1,10 @@
 package org.sonatype.nexus.blobstore.gcloud.internal;
 
+import java.util.Iterator;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
-import org.sonatype.nexus.blobstore.MockBlobStoreConfiguration;
 import org.sonatype.nexus.blobstore.api.BlobStoreConfiguration;
 import org.sonatype.nexus.blobstore.api.BlobStoreManager;
 import org.sonatype.nexus.blobstore.quota.BlobStoreQuotaSupport;
@@ -13,16 +13,19 @@ import org.sonatype.nexus.common.collect.NestedAttributesMap;
 import org.sonatype.nexus.pax.exam.NexusPaxExamSupport;
 import org.sonatype.nexus.testsuite.testsupport.NexusITSupport;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.junit.After;
 import org.junit.Test;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertThat;
-
 /**
  * Integration test intended to deploy the plugin within Nexus Repository manager to confirm that the OSGi
- * packaging is correct and the bundle will run.
+ * packaging is correct, the bundle will activate, and a Google Cloud BlobStore can be created.
+ *
+ * Depends on GOOGLE_APPLICATION_CREDENTIALS being present in your Environment.
  */
 public class GoogleCloudBlobStoreDeploymentIT
   extends GoogleCloudBlobStoreITSupport
@@ -30,6 +33,7 @@ public class GoogleCloudBlobStoreDeploymentIT
 
   private static final String uid = UUID.randomUUID().toString().substring(0,7);
   private static final String bucketName = "deployment-it-" + uid;
+  private BlobStoreConfiguration configuration;
 
   @Inject
   private BlobStoreManager blobStoreManager;
@@ -42,11 +46,14 @@ public class GoogleCloudBlobStoreDeploymentIT
     );
   }
 
+  /**
+   *
+   *
+   * @throws Exception
+   */
   @Test
   public void createGoogleCloudBlobStore() throws Exception {
-    assertThat(blobStoreManager, notNullValue());
-
-    BlobStoreConfiguration configuration = new MockBlobStoreConfiguration();
+    configuration = blobStoreManager.newConfiguration();
     configuration.setName("GoogleCloudBlobStoreDeploymentIT");
     // TODO GoogleCloudBlobStore.*_KEY is not visible since the class is in an internal package
     configuration.setType("Google Cloud Storage");
@@ -58,5 +65,20 @@ public class GoogleCloudBlobStoreDeploymentIT
     quotaMap.set(BlobStoreQuotaSupport.LIMIT_KEY, 512000L);
 
     blobStoreManager.create(configuration);
+  }
+
+  @After
+  public void destroyBucket() {
+    Storage storage = StorageOptions.newBuilder().build().getService();
+    log.debug("Deleting files from " + bucketName);
+    // must delete all the files within the bucket before we can delete the bucket
+    Iterator<Blob> list = storage.list(bucketName,
+        Storage.BlobListOption.prefix("")).iterateAll()
+        .iterator();
+    list.forEachRemaining(blob -> blob.delete());
+
+    storage.delete(bucketName);
+
+    log.info(bucketName + "bucket deleted");
   }
 }
